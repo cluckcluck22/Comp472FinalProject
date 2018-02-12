af9d8828d78da2c532d4911b4c69ea56ec45e042
Jesse Tsang <Jessetsang@outlook.com>
2018-02-11 20:05:40 -0500

    Removed unused imports.

--- candyCrisis/src/candyCrisis/FileHandler/FileHandler.java
+++ candyCrisis/src/candyCrisis/FileHandler/FileHandler.java
@@ -6,9 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
