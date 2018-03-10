d3208ee21f0a961055807615d5fdb76ae789c325
Jesse Tsang <JesseTsang@Outlook.com>
2018-03-10 17:38:15 -0500

    AIManager Class done-ish.
    Main feature complete (I think), but not properly test yet because
    stuff.

--- candyCrisis/src/candyCrisis/AI/AIManager.java
+++ candyCrisis/src/candyCrisis/AI/AIManager.java
@@ -1,6 +1,67 @@
 package candyCrisis.AI;
 
+import java.util.List;
+
+import candyCrisis.Dijkstra;
+import candyCrisis.Result;
+import candyCrisis.FileHandler.FileHandler;
+
 public class AIManager
 {
+	private Dijkstra solver;
+	private FileHandler fileHandler;
+	
+	private long startTime, totalTime;
+	
+	//Constructor
+	public AIManager()
+	{
+		this.solver = new Dijkstra();
+		this.fileHandler = new FileHandler();
+		
+		//Start counting total time for solving a puzzle.
+		this.startTime = System.nanoTime();
+		
+		startAI();
+	}
+	
+	//This method will ...
+	//1. Check if there is a board string available in the FileHandler object.
+	//2. While there is a board string 
+	//3. 	Pass the string to the Dijkstra object and invoke runDijkstra(boardString).
+	private void startAI()
+	{
+		boolean hasNextBoardString = false;
+		String boardString = "";
+		
+		//Check if there is a board available ...
+		hasNextBoardString = fileHandler.hasNextBoard();
+				
+		//While available ... do
+		while(hasNextBoardString)
+		{
+			boardString = fileHandler.getNextBoardString();
+			String resultPath = getResultString(solver.runDijkstra(boardString));
+			
+			long endTime = System.nanoTime();
+			
+			totalTime = startTime - endTime;
+			
+			Result result = new Result(resultPath, totalTime); 
+			
+			fileHandler.saveBoardResult(result);
+		}		
+	}
 
+	private String getResultString(List<String> result)
+	{
+		String resultString = "";
+		
+		for (String s : result)
+		{
+			resultString = resultString + s;
+		}
+		
+		return resultString;
+	}
 }
