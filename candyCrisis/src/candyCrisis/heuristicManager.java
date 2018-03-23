04d6202ca39dc78463c3c5c37938262b95736c26
Jennifer Sunahara <jennifer.sunahara@gmail.com>
2018-03-23 01:48:09 -0400

    Heuristic change and cleaning outputboard

--- candyCrisis/src/candyCrisis/heuristicManager.java
+++ candyCrisis/src/candyCrisis/heuristicManager.java
@@ -9,7 +9,7 @@ public class heuristicManager {
 
 	public static int evaluateHeuristic(String node)
 	{
-		int h = (SymmetryMatch.getSymmetryMatch1(node)) + 2*(SymmetryMatch.getSymmetryMatch2(node));
+		int h = (SymmetryMatch.getSymmetryMatch1(node)) + 3*(SymmetryMatch.getSymmetryMatch2(node));
 	    return h;
 	}
 }
